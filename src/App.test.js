3600cbf0cb0b5b57b2cce4a30ec8c2a1a94469bd
EL <ickyicky@gmail.com>
2021-05-30 16:43:25 +0900

    eslint, prettier, test 설정

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import '@testing-library/jest-dom';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders hello', () => {
+  const { container } = render(<App />);
+
+  expect(container).toHaveTextContent('Hello');
 });
